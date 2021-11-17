44a91188774188adab273b660eebaacabee0300b
Luis G Estrades <luisgarciaestrades@gmail.com>
2021-11-17 23:05:12 +0100

    Add aimharderservice (#6)
    * Add aimharderservice
    
    - Make script wait until desire time to start booking since cronjobs
      aren't precise enough
    
    * Update AimHarderService login to be a static method

--- constants.py
+++
@@ -1,3 +0,0 @@
-LOGIN_ENDPOINT = "https://aimharder.com/login"
-BOOK_ENDPOINT = "https://lahuellaaribau.aimharder.com/api/book"
-CLASSES_ENDPOINT = "https://lahuellaaribau.aimharder.com/api/bookings"
--- settings.py
+++ settings.py
@@ -1,5 +1,22 @@
-BOX_ID = 9316  # la huella aribau
-EMAIL = "your_email@domain.com"
-PASSWORD = "12345"
+ACCOUNT = {
+    "EMAIL": "your_email@domain.com",
+    "PASSWORD": "12345",
+}
 
-BOOKING_GOALS = ({0: "1730"},)  # mondays at 17:30
+# La huella aribau
+BOX = {
+    "ID": 9316,
+    "NAME": "lahuellaaribau",
+}
+
+BOOKING_GOALS = (
+    # mondays at 17:00
+    {"day_of_week": 0, "filters": {"timeid": "1700"}},
+)
+
+# Do not touch these
+ENDPOINTS = {
+    "LOGIN": "https://aimharder.com/login",
+    "BOOK": f"https://{BOX['ID']}.aimharder.com/api/book",
+    "CLASSES": f"https://{BOX['NAME']}.aimharder.com/api/bookings",
+}
