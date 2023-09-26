0282b8ae38227fb351694cfb23a5eb21e18a142a
Jose Ortiz <jortiz@nepgroup.com>
2023-09-26 18:22:49 +0200

    New argument

--- src/client.py
+++ src/client.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 from http import HTTPStatus
 
@@ -45,25 +46,28 @@ class AimHarderClient:
                 raise IncorrectCredentials
         return session
 
-    def get_classes(self, target_day: datetime):
+    def get_classes(self, target_day: datetime, family_id: str = None):
         response = self.session.get(
             classes_endpoint(self.box_name),
             params={
                 "box": self.box_id,
                 "day": target_day.strftime("%Y%m%d"),
-                "familyId": "",
+                "familyId": family_id,
+                "_": time.time(),
             },
         )
         return response.json().get("bookings")
 
-    def book_class(self, target_day: datetime, class_id: str) -> bool:
+    def book_class(
+        self, target_day: datetime, class_id: str, family_id: str = None
+    ) -> bool:
         response = self.session.post(
             book_endpoint(self.box_name),
             data={
                 "id": class_id,
                 "day": target_day.strftime("%Y%m%d"),
                 "insist": 0,
-                "familyId": "",
+                "familyId": family_id,
             },
         )
         if response.status_code == HTTPStatus.OK:
